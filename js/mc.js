3ac09b1006be87bff7db4683484f0ff0be1d456f
ANTONY VIJU A <51233263+AntonyViju@users.noreply.github.com>
2022-03-09 11:38:07 +0300

    Create mc.js

---
+++ js/mc.js
@@ -0,0 +1,1 @@
+<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/3d4c3e3cbba7eeae0fe2921b4/9c3fa38acd825e6e87289d375.js");</script>
